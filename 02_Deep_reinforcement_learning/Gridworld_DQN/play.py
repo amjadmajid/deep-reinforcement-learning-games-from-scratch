960a9d594995d6fa44bbcd712ac7d5cfa6cd4cfe
Amjad <amjad@AM-MBP.local>
2023-07-14 21:02:01 +0200

    minor update

--- 02_Deep_reinforcement_learning/Gridworld_DQN/play.py
+++ 02_Deep_reinforcement_learning/Gridworld_DQN/play.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     dqn.eval()
    
     env = GridWorld(shape = (5,5), obstacles = \
-                     [(4,1), (1,1), (2,1), (3,1),(2,3),(3,3),(4,3) ])
+                     [(0,1), (1,1), (4,1), (3,1),(2,3),(3,3),(4,3) ])
     state, info = env.reset()
     done = False
 
     for i in range(100):
         state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-        action = dqn(state).max(1)[1].view(1, 1)
-        next_state, reward, done, _  = env.step(action.item())
+        action = dqn(state).max(1)[1].view(1, 1).item()
+        next_state, reward, done, _  = env.step(action)
         state = next_state
         env.render()
         print(f"{action=}")
