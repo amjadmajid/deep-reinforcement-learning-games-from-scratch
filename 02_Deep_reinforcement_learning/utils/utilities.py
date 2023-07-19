04a3dd5fe793437d974c8095008197c915e9cf64
Amjad <amjad@AM-MBP.local>
2023-07-20 00:01:42 +0200

    plot episode duration

--- 02_Deep_reinforcement_learning/utils/utilities.py
+++ 02_Deep_reinforcement_learning/utils/utilities.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import torch
 
 def plot(scores):
     plt.gcf().clear()
@@ -8,3 +9,22 @@ def plot(scores):
     plt.plot(scores)
     plt.show(block=False)
     plt.pause(.1)
+
+# Plot duration curve: 
+# From http://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+episode_durations = []
+def plot_durations():
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.FloatTensor(episode_durations)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+        
+    plt.pause(0.001)  # pause a bit so that plots are updated
